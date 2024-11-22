18ac1a59f86ce91afce092101d2acbcef324ac00
Juntaroo <juliangodoy.epet20@gmail.com>
2024-11-22 15:52:53 -0300

    third commit

--- src/modelos/auth/auth.module.ts
+++ src/modelos/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../usuarios/users.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from '../usuarios/jwt.strategy/jwt.strategy';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { envs } from 'src/configuration';
 
@@ -12,7 +13,7 @@ import { envs } from 'src/configuration';
     secret: envs.jwt_secreta,
     signOptions: { expiresIn: "20h" },
   }),],
-  providers: [AuthService, UsersService, PrismaService],
+  providers: [AuthService, UsersService, PrismaService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
