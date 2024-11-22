361091e26d9c03b3ce606a2f72891c12c8eecafa
Juntaroo <juliangodoy.epet20@gmail.com>
2024-11-22 15:47:43 -0300

    second commit

--- src/modelos/usuarios/jwt.strategy/jwt.strategy.ts
+++ src/modelos/usuarios/jwt.strategy/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username }; //decodifica el token
+    return { id: payload.sub, correo: payload.correo }; //decodifica el token
   }
 }
